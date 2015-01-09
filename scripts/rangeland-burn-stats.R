cce0aa1018f581fe04f14a98feff330e41a3a08b
Dylan W. Schwilk <dylan@schwilk.org>
2015-01-09 16:14:06 -0600

    Add PCA investigation
    No evidence for relationship between biomass and plant measurements

--- scripts/rangeland-burn-stats.R
+++ scripts/rangeland-burn-stats.R
@@ -16,3 +16,26 @@ ptemp.sur.mod <- lme(ptemp.sur ~ spcode, random = ~ 1 | plot, data = burns)
 summary(ptemp.sur.mod)
 anova(ptemp.sur.mod)
 
+
+## PCAs
+library(pcaMethods)
+
+ndata <- burns[ rowSums( is.na(burns[, c(17:20) ]) ) != ncol(burns[, c(17:20)] ),]
+ndata <- ndata[ rowSums( is.na(ndata[, c(7:10,12:14) ]) ) != ncol(ndata[, c(7:10,12:14)] ),]
+
+plants <- ndata[, c(17:20)] # plant measurements
+fire <- ndata[, c(7:10, 12:14)] # HOBO temperatures
+
+plants.PCA <- pca(plants, nPcs=4, method="ppca", center=TRUE)
+fire.PCA <- pca(fire, nPcs=4, method="ppca", center=TRUE)
+
+summary(plants.PCA)
+summary(fire.PCA)
+
+slplot(fire.PCA)
+
+slplot(plants.PCA)
+
+allscores <- as.data.frame(cbind(scores(plants.PCA), scores(fire.PCA)))
+#names(allscores) <- c(paste(names(plants.PCA), "p", sep="_"), paste(names(fire.PCA), "f", sep="_"))
+plot(allscores)
